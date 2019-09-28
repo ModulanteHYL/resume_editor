9918280dc0c3883a4208b82c2271feb1afd8d05f
ModulanteHYL <13317634939@163.com>
2019-09-28 22:48:22 +0800

    添加提示以及支持回车键确定输入内容

--- src/components/edu_exp.vue
+++ src/components/edu_exp.vue
@@ -2,11 +2,11 @@
     <div class="edu_exp">
         <h3 class="hr">教育经历</h3>
         <div class="school">
-            <input class="list_box" v-if="!schoolName" type="text" placeholder="毕业院校..." @blur="schoolName=$event.target.value">
+            <input class="list_box" v-if="!schoolName" type="text" placeholder="毕业院校..." @blur="schoolName=$event.target.value" @keypress.enter="schoolName=$event.target.value">
             <span v-else @click="schoolName=''">{{schoolName}}</span>
-            <input class="list_box" v-if="!courseInfo" type="text" placeholder="专业名称--本科/专科..." @blur="courseInfo=$event.target.value">
+            <input class="list_box" v-if="!courseInfo" type="text" placeholder="专业名称--本科/专科..." @blur="courseInfo=$event.target.value" @keypress.enter="courseInfo=$event.target.value">
             <span v-else @click="courseInfo=''">{{courseInfo}}</span>
-            <input class="list_box" v-if="!studyTime" type="text" placeholder="起始年月-毕业年月..." @blur="studyTime=$event.target.value">
+            <input class="list_box" v-if="!studyTime" type="text" placeholder="起始年月-毕业年月..." @blur="studyTime=$event.target.value" @keypress.enter="studyTime=$event.target.value">
             <span v-else @click="studyTime=''">{{studyTime}}</span>
         </div>
     </div>
